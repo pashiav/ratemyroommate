2948e5b1d345edb08e1cec54158064d7a8e3aa0e
EdgarMC22 <143836543+EdgarMC22@users.noreply.github.com>
2025-04-06 03:36:46 -0500

    fixed page.tsx param typing

--- rmr/app/roommate/[id]/review/new/page.tsx
+++ rmr/app/roommate/[id]/review/new/page.tsx
@@ -1,7 +1,13 @@
-// app/roommate/[id]/review/new/page.tsx
+import { type Metadata } from 'next';
 import ReviewForm from './review-form';
 
-export default function NewReviewPage({ params }: { params: { id: string } }) {
+type PageProps = {
+  params: {
+    id: string;
+  };
+};
+
+export default function NewReviewPage({ params }: PageProps) {
   return (
     <main className="flex min-h-screen flex-col bg-blue-50 p-4">
       <div className="max-w-3xl mx-auto w-full my-8">
