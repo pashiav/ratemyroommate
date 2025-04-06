d0df050c67f0a32ad86169422f7d1e0defb12529
EdgarMC22 <143836543+EdgarMC22@users.noreply.github.com>
2025-04-06 02:15:07 -0500

    update

--- rmr/app/roommate/[id]/page.tsx
+++ rmr/app/roommate/[id]/page.tsx
@@ -1,15 +1,6 @@
 // app/roommate/[id]/page.tsx
 import RoommateDetails from './roommate-details-client';
 
-interface PageProps {
-  params: {
-    id: string;
-  };
-  searchParams: Record<string, string | string[] | undefined>;
-}
-
-export default function RoommatePage(props: PageProps) {
-  const { id } = props.params;
-  
-  return <RoommateDetails id={id} />;
+export default function RoommatePage(props: any) {
+  return <RoommateDetails id={props.params.id} />;
 }
