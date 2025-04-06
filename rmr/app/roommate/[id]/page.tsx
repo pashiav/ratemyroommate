02ca8573b7016a81874048ec5410a9a9cdef11b6
EdgarMC22 <143836543+EdgarMC22@users.noreply.github.com>
2025-04-06 02:16:50 -0500

    update

--- rmr/app/roommate/[id]/page.tsx
+++ rmr/app/roommate/[id]/page.tsx
@@ -1,6 +1,6 @@
-// app/roommate/[id]/page.tsx
 import RoommateDetails from './roommate-details-client';
 
 export default function RoommatePage(props: any) {
+  // Just pass the ID directly without destructuring
   return <RoommateDetails id={props.params.id} />;
 }
