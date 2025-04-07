c5415b4748428e82ce656ead74afb1fb891bdf15
pashiav <pashiavang2022@gmail.com>
2025-04-07 17:35:15 -0500

    refactor code and pretty ui for landing page

--- rmr/app/page.tsx
+++ rmr/app/page.tsx
@@ -48,116 +48,116 @@ export default function Home() {
 
   return (
     <main className="min-h-screen bg-[#315d8d] p-4">
-      <TopFridge>
-        <AuthHeader />
-        {/* To Do List Section */}
-        <div className="mt-10 mx-auto max-w-lg relative">
+      {/* To Do List Section */}
+      <div className="relative z-20 flex justify-center w-full">
+        <div className="w-full max-w-[500px] mt-[50vh] absolute border-2 border-navy-blue rounded-lg p-6 pt-0 pb-[25vh] space-y-4 bg-[#fdfeff] shadow-lg">
+          {/* Pink magnets */}
           <div
-            className="border-2 border-navy-blue rounded-lg p-6 pt-0 pb-60 space-y-4"
-            style={{ background: "#fdfeff" }}
+            className="absolute -left-10 -top-6 w-20 h-20 rounded-full border-2"
+            style={{
+              background:
+                "radial-gradient(circle at center, #e3a4a4 50%, #cd9292 98%)",
+            }}
+          />
+          <div
+            className="absolute -right-10 -top-9 w-20 h-20 rounded-full border-2"
+            style={{
+              background:
+                "radial-gradient(circle at center, #e3a4a4 50%, #cd9292 98%)",
+            }}
+          />
+          {/* TO DO Heading */}
+          <h2
+            className="text-xl font-medium text-center underline"
+            style={{ color: "#a0a6bf" }}
           >
-            {/* Pink magnets */}
-            <div
-              className="absolute -left-10 -top-6 w-20 h-20 rounded-full border-2"
-              style={{
-                background:
-                  "radial-gradient(circle at center, #e3a4a4 50%, #cd9292 98%)",
-              }}
+            TO DO LIST
+          </h2>
+
+          {/* Sign up task */}
+          <div className="flex items-start space-x-4 pt-0">
+            <Checkbox
+              id="signup"
+              checked={todoItems[0].checked}
+              onCheckedChange={() => handleToggle("signup")}
+              className="text-3xl font-bold text-primary mt-7"
             />
-            <div
-              className="absolute -right-10 -top-9 w-20 h-20 rounded-full border-2"
-              style={{
-                background:
-                  "radial-gradient(circle at center, #e3a4a4 50%, #cd9292 98%)",
-              }}
-            />
+            <div className="flex-1">
+              <label
+                htmlFor="signup"
+                className="text-5xl mt-5 block font-bold text-darkblue"
+              >
+                {todoItems[0].label}
+              </label>
 
-            {/* TO DO Heading */}
-            <h2
-              className="text-xl font-medium text-center underline"
-              style={{ color: "#76708d" }}
-            >
-              TO DO LIST
-            </h2>
-
-            {/* Sign up task */}
-            <div className="flex items-start space-x-4 pt-0">
-              <Checkbox
-                id="signup"
-                checked={todoItems[0].checked}
-                onCheckedChange={() => handleToggle("signup")}
-                className="text-3xl font-bold text-primary mt-7"
-              />
-              <div className="flex-1">
-                <label
-                  htmlFor="signup"
-                  className="text-5xl mt-5 block font-bold text-darkblue"
-                >
-                  {todoItems[0].label}
-                </label>
-
-                <div className="flex">
-                  <Input
-                    placeholder="Enter your .edu email address"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                    className="border-2 border-darkblue rounded-md py-2 px-4"
-                  />
-                  <SignUpButton 
+              <div className="flex">
+                <Input
+                  placeholder="Enter your .edu email address"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  className="border-2 border-darkblue rounded-md py-2 px-4"
+                />
+                <SignUpButton
                   mode="modal"
                   initialValues={{
                     emailAddress: email,
                   }}
+                >
+                  <Button
+                    onClick={handleGoClick}
+                    className="ml-2 bg-darkblue text-white rounded-md px-4"
+                    disabled={!email.endsWith(".edu")}
                   >
-                    <Button
-                      onClick={handleGoClick}
-                      className="ml-2 bg-darkblue text-white rounded-md px-4"
-                      disabled={!email.endsWith(".edu")}
-                    >
-                      GO
-                    </Button>
-                  </SignUpButton>
-                  {showError && (
+                    GO
+                  </Button>
+                </SignUpButton>
+                {showError && (
                   <p className="text-red-600 text-sm ml-1">
                     Please enter a valid <span className="underline">.edu</span>{" "}
                     email address!
                   </p>
                 )}
-                </div>
-
-                
               </div>
             </div>
-
-            {/* Other tasks */}
-            {todoItems.slice(1).map((item) => (
-              <div key={item.id} className="flex items-center space-x-4 mt-2">
-                <Checkbox
-                  id={item.id}
-                  disabled={item.id === "dishes"}
-                  checked={item.checked}
-                  onCheckedChange={() => handleToggle(item.id)}
-                  className="w-6 h-6"
-                />
-                <label
-                  htmlFor={item.id}
-                  className="text-2xl font-mediu"
-                  style={{ color: "#76708d" }}
-                >
-                  {item.label}
-                </label>
-              </div>
-            ))}
           </div>
+
+          {/* Other tasks */}
+          {todoItems.slice(1).map((item) => (
+            <div key={item.id} className="flex items-center space-x-4 mt-2">
+              <Checkbox
+                id={item.id}
+                disabled={item.id === "dishes"}
+                checked={item.checked}
+                onCheckedChange={() => handleToggle(item.id)}
+                className="w-6 h-6"
+              />
+              <label
+                htmlFor={item.id}
+                className="text-2xl font-medium"
+                style={{ color: "#a0a6bf" }}
+              >
+                {item.label}
+              </label>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <TopFridge>
+        <AuthHeader />
+        <div className="logo flex justify-center">
+          <img
+            src="/logo.png"
+            alt="Logo"
+            className="w-[30rem] h-auto max-w-full"
+          />
         </div>
       </TopFridge>
 
       <BottomFridge>
-        <div className="mt-32 bg-[#fdfeff] rounded-[10px] shadow-md p-8 max-w-xl mx-auto text-left font-sans text-[#315d8d] italic">
-          <h2 className="text-6xl font-bold mb-4 text-center">
-            About RMR
-          </h2>
-          <p className="text-md">
+        <div className="mt-[30vh] bg-[#fdfeff] rounded-[10px] shadow-md p-8 max-w-xl mx-auto text-left text-[#315d8d] italic">
+          <h2 className="text-6xl font-bold mb-4 text-center">About RMR</h2>
+          <p className="text-md font-sans">
             We’ve all had unpredictable roommates - and wished we knew more
             before moving in.
             <br />
--- rmr/components/TopFridge.tsx
+++ rmr/components/TopFridge.tsx
@@ -3,10 +3,22 @@ import React from "react";
 export default function FridgeLayout({ children }: { children: React.ReactNode }) {
   return (
     // add the top part of the fridge, the shine on the left and right, the handle, and the content in the middle
-    <div className="bg-[#f5f5f5] rounded-[96px] shadow-lg p-8 min-h-screen relative">
-      <img src="/handle.png" alt="Fridge Handle" className="absolute left-0 ml-24 top-1/3 w-36 h-auto object-contain" />
-      <img src="/shine-left.png" alt="shine" className="absolute left-0 top-0 mt-6 ml-6 w-xs h-auto object-contain" />
-      <img src="/shine-right.png" alt="shine" className="absolute right-0 top-0 mt-6 mr-6 w-xs h-auto object-contain" />
+    <div className="bg-[#f5f5f5] rounded-[96px] shadow-lg p-8 min-h-screen relative z-0 overflow-hidden">
+      <img
+        src="/handle.png"
+        alt="Fridge Handle"
+        className="absolute left-0 ml-24 top-0 mt-[12vh] w-36 h-auto object-contain z-10"
+      />
+      <img
+        src="/shine-left.png"
+        alt="shine"
+        className="absolute left-0 top-0 mt-3 ml-3 w-xs h-auto object-contain z-10"
+      />
+      <img
+        src="/shine-right.png"
+        alt="shine"
+        className="absolute right-0 top-0 mt-3 mr-3 w-xs h-auto object-contain z-10"
+      />
       {children}
     </div>
   );
