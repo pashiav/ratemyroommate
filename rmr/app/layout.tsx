aa06d86ce8b1130cb3c1a975dbf24ca1aa813beb
pashiav <pashiavang2022@gmail.com>
2025-04-29 13:46:41 -0500

    remove deprecated code

--- rmr/app/layout.tsx
+++ rmr/app/layout.tsx
@@ -39,8 +39,6 @@ export default function RootLayout({
           },
         },
       }}
-      afterSignInUrl="/search"
-      afterSignUpUrl="/search"
     >
       <html lang="en">
         <body style={{ fontFamily: "'Lazy Dog', sans-serif" }}>
