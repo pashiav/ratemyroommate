329c570b125d2361352408ba9c607a2284730382
pashiav <pashiavang2022@gmail.com>
2025-05-19 22:58:57 -0500

    create css of fridge shine, removing images

--- rmr/components/BottomFridge.tsx
+++ rmr/components/BottomFridge.tsx
@@ -15,19 +15,26 @@ export default function BottomFridge({
         <img
           src="/handle-short.png"
           alt="Fridge Handle"
-          className="absolute left-0 ml-24 top-0 mt-24 w-36 h-auto object-contain z-0"
+          className="absolute left-0 ml-24 top-0 mt-24 w-36 h-auto object-contain z-0 pointer-events-none"
         />
-        <img
-          src="/shine-left.png"
-          alt="shine"
-          className="absolute left-0 top-0 mt-1 ml-1 w-xs h-auto object-contain z-0"
-        />
-        <img
-          src="/shine-right.png"
-          alt="shine"
-          className="absolute right-0 top-0 mt-1 mr-1 w-xs h-auto object-contain z-0"
-        />
-
+        {/* Left Shine */}
+        <div
+          className="absolute left-[1rem] top-[0.7rem] w-48 h-48 rounded-full border-[8px] border-white pointer-events-none"
+          style={{
+            clipPath: "ellipse(25% 25% at 30% 30%)",
+          }}
+        ></div>{" "}
+        <div className="absolute left-[1.25rem] top-[6.5em] h-[75%] w-[8.5px] bg-white rounded-full pointer-events-none"></div>
+        <div className="absolute left-[6.5rem] top-[.8rem] w-[3rem] h-[7px] bg-white rounded-full pointer-events-none"></div>
+        {/* Right Shine */}
+        <div
+          className="absolute right-[1rem] top-[0.7rem] w-48 h-48 rounded-full border-[8px] border-white pointer-events-none"
+          style={{
+            clipPath: "ellipse(25% 25% at 70% 30%)",
+          }}
+        ></div>{" "}
+        <div className="absolute right-[1.25rem] top-[6.5em] h-[75%] w-[8.5px] bg-white rounded-full pointer-events-none"></div>
+        <div className="absolute right-[6.5rem] top-[.8rem] w-[3rem] h-[7px] bg-white rounded-full pointer-events-none"></div>
         {children}
       </div>
 
--- rmr/components/LandingTopFridge.tsx
+++ rmr/components/LandingTopFridge.tsx
@@ -8,7 +8,7 @@ export default function LandingTopFridge({
   return (
     // add the top part of the fridge, the shine on the left and right, the handle, and the content in the middle
     <div
-      className="bg-[#f5f5f5] rounded-[96px] shadow-lg p-8 min-h-[50rem] relative z-0 overflow-hidden"
+      className="bg-[#f5f5f5] rounded-[96px] shadow-lg p-8 min-h-[50rem] pointer-events-none relative z-0 overflow-hidden"
       style={{ boxShadow: "0 4px 4px -2px rgba(0, 0, 0, 0.2)" }}
     >
       <img
@@ -16,16 +16,24 @@ export default function LandingTopFridge({
         alt="Fridge Handle"
         className="absolute left-0 ml-24 top-0 mt-40 w-36 h-auto object-contain z-10"
       />
-      <img
-        src="/shine-left-crop.png"
-        alt="shine"
-        className="absolute left-0 top-0 mt-2 ml-1 w-xs h-auto object-contain z-10"
-      />
-      <img
-        src="/shine-right-crop.png"
-        alt="shine"
-        className="absolute right-0 top-0 mt-1 mr-1 w-xs h-auto object-contain z-10"
-      />
+      {/* Left Shine */}
+      <div
+        className="absolute left-[1rem] top-[0.7rem] w-48 h-48 rounded-full border-[8px] border-white pointer-events-none"
+        style={{
+          clipPath: "ellipse(25% 25% at 30% 30%)",
+        }}
+      ></div>{" "}
+      <div className="absolute left-[1.25rem] top-[6.5em] h-[75%] w-[8.5px] bg-white rounded-full pointer-events-none"></div>
+      <div className="absolute left-[6.5rem] top-[.8rem] w-[3rem] h-[7px] bg-white rounded-full pointer-events-none"></div>
+      {/* Right Shine */}
+      <div
+        className="absolute right-[1rem] top-[0.7rem] w-48 h-48 rounded-full border-[8px] border-white pointer-events-none"
+        style={{
+          clipPath: "ellipse(25% 25% at 70% 30%)",
+        }}
+      ></div>{" "}
+      <div className="absolute right-[1.25rem] top-[6.5em] h-[75%] w-[8.5px] bg-white rounded-full pointer-events-none"></div>
+      <div className="absolute right-[6.5rem] top-[.8rem] w-[3rem] h-[7px] bg-white rounded-full pointer-events-none"></div>
       {children}
     </div>
   );
