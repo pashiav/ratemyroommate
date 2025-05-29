3005b65970d2ecf0aee9394c225444fa9667d1a6
pashiav <pashiavang2022@gmail.com>
2025-05-29 10:42:56 -0500

    add css to remove images

--- rmr/components/BottomFridge.tsx
+++ rmr/components/BottomFridge.tsx
@@ -8,24 +8,44 @@ export default function BottomFridge({
   return (
     <div className="w-full">
       <div
-        className="bg-lightGray rounded-[96px] mt-0 shadow-lg p-10 min-h-[55rem] w-[100%] mx-auto relative"
-        style={{ boxShadow: "0 -4px 4px -2px rgba(0, 0, 0, 0.2)" }}
-      >
-        <img
-          src="/handle-short.png"
-          alt="Fridge Handle"
-          className="absolute left-0 ml-24 top-0 mt-24 w-36 h-auto object-contain z-0"
+      className="bg-[#f5f5f5] rounded-[96px] shadow-lg p-8 min-h-[50rem] relative z-0 overflow-hidden"
+      style={{ boxShadow: "0 4px 4px -2px rgba(0, 0, 0, 0.2)" }}
+    >
+      {/* Handle */}
+      <div className="absolute w-auto h-[35rem] left-0 ml-20 top-0 mt-[5rem]">
+        {/* Main handle body */}
+        <div className="w-[4.75rem] h-full border-r-[2rem] border-l-[.5rem] border-t-[2.25rem] border-b-[2.25rem] border-darkBlue relative rounded-[2rem]"></div>
+        {/* Handle top */}
+        <div
+          className="absolute top-0 left-[2.5rem] w-[4.5rem] h-[4rem] bg-darkBlue rounded-rl-[0.5rem] rounded-tr-[1.5rem]"
+          style={{
+            clipPath: "polygon(0% 0%, 100% 0%, 85% 100%, 10% 100%)",
+          }}
         />
-        <img
-          src="/shine-left.png"
-          alt="shine"
-          className="absolute left-0 top-0 mt-1 ml-1 w-xs h-auto object-contain z-0"
-        />
-        <img
-          src="/shine-right.png"
-          alt="shine"
-          className="absolute right-0 top-0 mt-1 mr-1 w-xs h-auto object-contain z-0"
-        />
+        {/* Handle bottom */}
+        <div
+          className="absolute bottom-0 left-[2.5rem] w-[4.5rem] h-[3.5rem] bg-darkBlue rounded-rl-[0.5rem] rounded-br-[1.5rem]"
+          style={{
+            clipPath: "polygon(15% 0%, 90% 0%, 100% 100%, 0% 100%)",
+          }}
+        ></div>
+      </div>
+      {/* Left Shine */}
+      <div
+        className="absolute left-[1rem] top-[0.7rem] w-48 h-48 rounded-full border-[8px] border-white"
+        style={{
+          clipPath: "ellipse(25% 25% at 30% 30%)",
+        }}
+      ></div>{" "}
+      <div className="absolute left-[1.25rem] top-[6.5em] h-[75%] w-[8.5px] bg-white rounded-full"></div>
+      <div className="absolute left-[6.5rem] top-[.8rem] w-[3rem] h-[7px] bg-white rounded-full"></div>
+      {/* Right Shine */}
+      <div
+        className="absolute right-[1rem] top-[0.7rem] w-48 h-48 rounded-full border-[8px] border-white"
+        style={{
+          clipPath: "ellipse(25% 25% at 70% 30%)",
+        }}
+      ></div>{" "}
 
         {children}
       </div>
